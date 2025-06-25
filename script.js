065d9c6dd64bde4d3e1ea5d3acdc7f0ae8cedcb9
Maushammishra843106 <maushamkumari546@gmail.com>
2025-06-25 22:09:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,1 @@
-let cart = JSON.parse(localStorage.getItem('cart')) || [];
 
-document.querySelectorAll('.add-to-cart-btn').forEach(button => {
-  button.addEventListener('click', () => {
-    const item = {
-      id: button.dataset.id,
-      name: button.dataset.name,
-      price: parseFloat(button.dataset.price),
-    };
-    cart.push(item);
-    localStorage.setItem('cart', JSON.stringify(cart));
-    alert(${item.name} added to cart!);
-  });
-});
